65846019dde7eaa18386454430a48b881fd36c55
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2017-12-18 22:52:49 +0530

    Create prog6.c

---
+++ beginnerprograms/set1/prog6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+int year;
+scanf("%d",&year);
+if(year%400==0)
+printf("leap year");
+else if(year%100==0)
+printf("not a leap year");
+else if(year%4==0)
+printf("leap year");
+else
+printf("not a leap year");
+
+	return 0;
+}
