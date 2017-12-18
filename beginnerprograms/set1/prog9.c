fd3aa62894d87c9c978068dcf1d4d48e93cb9a93
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2017-12-18 23:27:37 +0530

    Update prog9.c

--- beginnerprograms/set1/prog9.c
+++ beginnerprograms/set1/prog9.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-int n,sum=0,result=0;
+int n,v=0,sum=0;
 scanf("%d",&n);
-for(int i=0;i<=n;i++)
+for(int j=0;j<=n;j++)
 {
-result=result+sum;
-sum++;
+sum=sum+v;
+v++;
 }
-printf("%d",result);
-	return 0;
+printf("%d",sum);
+return 0;
 }
