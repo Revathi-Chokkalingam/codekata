a13ffeabc8043b0e3685141fce5b84bb3d200c76
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2018-01-08 23:01:14 +0530

    Create maxarrayprog22.c

---
+++ beginnerprograms/set3/maxarrayprog22.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+long int N,a[100000];
+long int i,temp;
+scanf("%ld",&N);
+for(i=0;i<N;i++)
+{
+	scanf("%ld",&a[i]);
+	temp=a[0];
+}
+for(i=0;i<N;i++)
+{
+	if(a[i]>a[0])
+	temp=a[i];
+}
+printf("%ld",temp);
+	return 0;
+}
