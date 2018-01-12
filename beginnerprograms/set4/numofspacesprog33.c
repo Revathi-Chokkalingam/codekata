bcda5d72e84f0b1710ff00d729ddf42363a9f09a
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2018-01-12 12:12:26 +0530

    Create numofspacesprog33.c

---
+++ beginnerprograms/set4/numofspacesprog33.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	char ch[100];
+	int i,k,count=0;
+	scanf("%[^\n]s",ch);
+	for(k=0;ch[k]!='\0';++k)
+	{
+		
+	}
+	for(i=0;i<k;i++)
+	{
+	
+	if(ch[i]==' ')
+	{
+		count++;
+	}
+		
+	}
+	printf("%d",count);
+	return 0;
+}
