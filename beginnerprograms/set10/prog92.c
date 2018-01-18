7abda3e6b31cad7969d0e7f3eb138ba720e867f0
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2018-01-18 21:55:13 +0530

    Create prog92.c

---
+++ beginnerprograms/set10/prog92.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+int N,a[100],i,sum=0;
+scanf("%d",&N);
+for(i=0;i<N;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=0;i<N;i++)
+{
+	sum=sum+a[i];
+}
+printf("%d",sum);
+	return 0;
+}
