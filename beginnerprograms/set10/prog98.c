a9f1ccfc8d28f4c6138c701cbadc0285282ee2f1
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2018-03-08 21:02:54 +0530

    Update prog98.c

--- beginnerprograms/set10/prog98.c
+++ beginnerprograms/set10/prog98.c
@@ -2,20 +2,18 @@
 
 int main(void) {
 	// your code goes here
-	int n,a[100],i;
+	int n,a[100],i,d;
 	scanf("%d",&n);
-	for(i=0;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
 		scanf("%d",&a[i]);
 	}
-	for(i=0;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
-		if(a[i+1]-a[i]!=1)
-		{
-			a[i+1]=a[i]+1;
-		}
-		printf("%d ",a[i]);
+	if(a[i]!=i)
+	printf("%d",i);
 	}
+	
 	
 	return 0;
 }
