4d086fbf73ca19c649f70fb50b26c993d242f0f9
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2018-03-09 23:36:32 +0530

    Update prog6.c

--- hunter/set1/prog6.c
+++ hunter/set1/prog6.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-	long int n,i,a[100000],freq[100000],q,p;
+	long int n,i,a[100000],freq[100000],q,p,count=0;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
@@ -18,15 +18,27 @@ int main(void) {
 			freq[a[i]]++;
 		}
 	}
+	
 	for(q=0;q<n;q++)
 	{
-		if(freq[q]>1)
+		if(freq[q]<=1)
+		{
+			count++;
+		}
+		else if(freq[q]>1)
 		{
 			p=q;
 			break;
 		}
 	}
+	if(count==n)
+	{
+		printf("unique");
+	}
+	else
+	{
 	printf("%d",p);
+	}
 	return 0;
 }
 
