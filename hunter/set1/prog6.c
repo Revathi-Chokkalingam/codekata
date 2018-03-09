f9705b657fb7983621765e9cb009651ed6b45461
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2018-03-09 23:10:11 +0530

    Create prog6.c

---
+++ hunter/set1/prog6.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) {
+	long int n,i,a[100000],freq[100000],q,p;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(q=0;q<n;q++)
+	{
+		freq[q]=0;
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>=0&&a[i]<=9999)
+		{
+			freq[a[i]]++;
+		}
+	}
+	for(q=0;q<n;q++)
+	{
+		if(freq[q]>1)
+		{
+			p=q;
+			break;
+		}
+	}
+	printf("%d",p);
+	return 0;
+}
+
