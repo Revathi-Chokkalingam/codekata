181fade79b077eabc712e0130994ce5041877359
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2018-05-17 21:33:25 +0530

    Create prog7.c

---
+++ hunter/set1/prog7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n,i,a[100];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	if((i%2==0)&&(a[i]%2!=0))
+	{
+		printf("%d",a[i]);
+	}
+	else if((i%2!=0)&&(a[i]%2==0))
+	{
+		printf("%d",a[i]);
+	}
+	}
+	return 0;
+}
