5c2efbbd1b4219e3ef0ebd56a35a2463957c2b1d
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2018-05-17 23:08:31 +0530

    Create prog9.c

---
+++ hunter/set1/prog9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n,m,i,a[100],j,b[100],k=0,temp,x;
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+
+for(i=0;i<n;i++)
+{
+	for(j=i+1;j<n;j++)
+	{
+		if(a[i]+a[j]==0)
+		printf("%d %d",a[i],a[j]);
+	}
+}
+
+	return 0;
+}
